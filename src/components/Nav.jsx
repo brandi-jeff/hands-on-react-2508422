c0f79ffbd98ea6137b0deb14fd566d014ae2a40f
Brandi Jefferson <112588194+brandi-jeff@users.noreply.github.com>
2023-03-19 02:08:59 +0000

    adding nav bar with characters list

---
+++ src/components/Nav.jsx
@@ -0,0 +1,28 @@
+export default ({cast, onChoice}) => {
+  return (
+    <nav className="container">
+      <ul>
+        <li>
+          <details role="list">
+            <summary role="list" aria-haspopup="listbox">cast</summary>
+            <ul role="listbox">
+              {cast.map(member => (
+                <li key={member.id}>
+                  <a onClick={() => {onChoice(member)}} 
+                  data-tooltip={member.name}>{member.name}</a>
+                </li>
+              ))}
+            </ul>
+          </details>
+          </li>
+      </ul>
+      <ul>
+        <li>
+          <a href="#">
+            <img style={{height:"50px"}} src="images/logo_bug_stargazers.svg" alt="StarGazers Logo" />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
