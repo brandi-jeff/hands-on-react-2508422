a118de6b3f670f62a22d8c156c49f4690d596554
Brandi Jefferson <112588194+brandi-jeff@users.noreply.github.com>
2023-03-22 02:17:39 +0000

    changes

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -4,7 +4,7 @@ const pageTitle = document.title;
 export default () => {
   const [count, setCount] = useState(0);
   useEffect(() => {
-    count && (document.title = `${pageTitle}--${count}`)
+    count && (document.title = `${pageTitle} ${count}`)
   })
   return (
     <button className="outline" onClick={() => setCount(count + 1)}>
